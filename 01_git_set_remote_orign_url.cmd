4814201888a9d19e7531be1b87bcd32f55a86dd8
PyraN-Ranjith Jayawardana <pyranranjith@gmail.com>
2024-02-24 12:27:42 +0530

    03. . cmd added

--- 01_git_set_remote_orign_url.cmd
+++ 01_git_set_remote_orign_url.cmd
@@ -1,10 +1,12 @@
 @echo off
 ::___________Set Variables______________________
 :: Origin for this repo
-::  set this_repo=https://github.com/Pyran-Ranjith/pm1_new.git
+  set this_repo=https://github.com/Pyran-Ranjith/pm1_new.git
 ::____________Process begin here________________________
 ::echo Hear is the result_______________________
-::set origin
-    git remote set-url origin %this_repo%
+::set origin 1. after clone to create local repo 
+    ::git remote set-url origin %this_repo%
+::set origin 2. Otherwise (after creade local git manually)
+    git remote add origin %this_repo%
 ::disply origin
     git remote -v
--- 03_git_push.cmd
+++ 03_git_push.cmd
@@ -1,9 +1,10 @@
 @echo off
 ::___________Set Variables______________________
-:: Origin for this repo
-:: Commit msg
-    set commit_msg="03. . cmd added"
+:: Branch
+set branch="master"
 ::____________Process begin here________________________
 ::echo Hear is the result_______________________
-git add .
-git commit -m %commit_msg%
+git branch -M %branch%
+git checkout %branch%
+git push -u origin %branch%
+
